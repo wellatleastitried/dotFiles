13d9c14c19ccfb17649f3c1eeeec02404e4a42d0
walitsnb <jackson1swindell@gmail.com>
2024-06-17 18:39:20 -0500

    Updated configs

---
+++ zsh/aliases.zsh
@@ -0,0 +1,9 @@
+MY_HOME='/home/walitsnb'
+
+alias sudo='sudo '
+alias sublist3r="python3 $MY_HOME/Tools/Sublist3r/sublist3r.py"
+alias gobuster="$MY_HOME/Tools/gobuster"
+alias burpsuite="$MY_HOME/Tools/BurpSuiteCommunity/BurpSuiteCommunity"
+alias zed='zeditor'
+# alias xsser='$HOME/Tools/xsser/xsser'
+alias updateConfigRepo="$MY_HOME/.config/scripts/dotFiles.sh"
