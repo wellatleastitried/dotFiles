259518134eae17f27125d12fe62f0e9098452e5d
walitsnb <jackson1swindell@gmail.com>
2024-06-20 19:02:27 -0500

    Updated configs

--- zsh/keybinds.zsh
+++ zsh/keybinds.zsh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# Enter current editor here
 EDITOR='zed'
 
+# Fuzzy finder for all files to open in editor
 fzf_widget() {
     CURRENT_DIR=$(pwd)
     cd
@@ -15,6 +17,18 @@ fzf_widget() {
 zle -N fzf_widget
 bindkey '^F' fzf_widget
 
+# Fuzzy finder for current directory
+fzf_local() {
+    FILE="$(find . -type f | fzf)"
+    if [ -n "$FILE" ]; then
+        eval $EDITOR "$FILE"
+    fi
+    zle reset-prompt
+}
+zle -N fzf_local
+bindkey '^H' fzf_local
+
+# Clear terminal with keybind
 clear_widget() {
     clear
     zle reset-prompt
