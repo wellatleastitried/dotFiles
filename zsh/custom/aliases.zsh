8a4fbaa5f30df10238e59cfd8c0ca31710b4a403
walitsnb <jackson1swindell@gmail.com>
2024-07-01 02:02:37 -0500

    Updated configs

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -7,8 +7,8 @@ alias gobuster="$MY_HOME/Tools/gobuster"
 alias burpsuite="$MY_HOME/Tools/BurpSuiteCommunity/BurpSuiteCommunity"
 alias zed='zeditor'
 alias updateConfigRepo="$MY_HOME/.config/scripts/dotFiles.sh"
-alias quickScan="$MY_HOME/.config/scripts/port_scanner.pl"
-alias wifiCLI="$MY_HOME/.config/scripts/wifiHandler.sh"
-alias checkEmail="$MY_HOME/.config/scripts/unreadEmail.rb"
+alias quickScan="$MY_HOME/.config/scripts/port_scanner"
+alias wifiCLI="$MY_HOME/.config/scripts/wifiHandler"
+alias checkEmail="$MY_HOME/.config/scripts/unreadEmail"
 
 source $REMOTE_TOOLS/kali.sh
