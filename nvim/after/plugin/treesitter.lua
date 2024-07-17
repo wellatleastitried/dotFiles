198674141297fb68c7189ecbe3eb410c058d48bb
walitsnb <jackson1swindell@gmail.com>
2024-07-17 01:55:30 -0500

    Updated configs

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-  ensure_installed = { "javascript", "typescript", "java", "perl", "bash", "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+  ensure_installed = { "javascript", "typescript", "ruby", "python", "java", "perl", "bash", "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
 
   sync_install = false,
 
