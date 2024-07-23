d0802f391736e96fa71ce929ce9da97f86f8eefb
walitsnb <jackson1swindell@gmail.com>
2024-07-23 17:49:36 -0500

    Updated configs

--- nvim/lua/walitsnb/remap.lua
+++ nvim/lua/walitsnb/remap.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>mb", ":Dispatch mvn clean package<CR>", { noremap = true,  silent = true })
-vim.keymap.set("n", "<leader>msr", ":term /home/walitsnb/.config/nvim/lua/walitsnb/runTargetJAR.sh<CR>")
+vim.keymap.set("n", "<leader>msr", ":term /home/walitsnb/.config/nvim/scripts/runTargetJAR.sh<CR>")
 
--- nvim/scripts/runTargetJAR.sh
+++ nvim/scripts/runTargetJAR.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-jar_file=$(find target -maxdepth 1 -type f -name "*.jar" | head -n 1)
+jar_file=$(find target -maxdepth 1 -type f -name "*.jar" ! -name "original*.jar" | head -n 1)
 echo "$jar_file"
 if [ -z "$jar_file" ]; then
     echo "No JAR files are located in the target directory."
