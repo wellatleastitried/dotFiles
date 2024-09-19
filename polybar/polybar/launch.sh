08fc4ccb53625bef465e5c5b5bdb053801f57345
walitsnb <jackson1swindell@gmail.com>
2024-09-19 18:08:56 -0500

    Updated configs

--- polybar/polybar/launch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Terminate any bar instances that are already running
-polybar-msg cmd quit
-
-echo "---" | tee -a /tmp/polybar.log
-polybar 2>&1 | tee -a /tmp/polybar.log & disown
-echo "Bar launched!"
