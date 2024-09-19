c37ae57db1493d71ddf0fecf312eb5a5212c4b44
walitsnb <jackson1swindell@gmail.com>
2024-09-19 18:15:55 -0500

    Updated configs

--- install/dependencies.sh
+++ install/dependencies.sh
@@ -16,14 +16,35 @@ command_exists() {
     command -v "$1" &> /dev/null
 }
 
+sudo pacman -Syu --noconfirm
+
+if ! command_exists git; then
+    echo "git not installed: installing now..."
+    sudo pacman -S --needed --noconfirm git base-devel
+    git config --global init.defaultBranch main
+    echo "git has been installed."
+else
+    echo "git is already installed."
+fi
+
 if ! command_exists yay; then
     echo "yay not installed: installing now..."
-    sudo pacman -Syu --needed git base-devel
-    git clone https://aur.archlinux.org/yay.git
-    cd yay || echo "Error installing yay, exiting..." && exit
-    makepkg -si
+    if ping -c 4 google.com; then
+        echo "Pulling yay from arch repo..."
+        git clone https://aur.archlinux.org/yay.git
+    else
+        echo "Copying yay from /tmp"
+        echo "TEMP DIRECTORY CONTENTS:" && ls /tmp
+        echo "YAY DIRECTORY CONTENTS:" && ls /tmp/yay
+        echo "PWD IS $(pwd)"
+        echo "CURRENT DIRECTORY CONTENTS:" && ls || echo "Could not list pwd" && exit 1
+        mv /tmp/yay /workspace/yay || echo "yay not found in /tmp, exiting..." && exit 1
+    fi
+    echo "Current Directory contents:" && ls
+    cd yay || echo "Error installing yay, exiting..." && exit 1
+    makepkg -si || echo "Unable to make package for yay, exiting..." && exit 1
     cd ..
-    rm -rf yay
+    sudo rm -rf yay || echo "Error removing files, exiting..." && exit 1
     echo "yay has been installed."
 else
     echo "yay is already installed."
@@ -31,7 +52,7 @@ fi
 
 if ! command_exists perl; then
     echo "Perl not installed: installing now..."
-    sudo pacman -S --needed perl perl-term-extendedcolor
+    sudo pacman -S --needed --noconfirm perl perl-term-extendedcolor
     echo "Perl has been installed"
 else
     echo "Perl is already installed"
@@ -39,7 +60,7 @@ fi
 
 if ! command_exists qrencode; then
     echo "qrencode not installed: installing now..."
-    sudo pacman -S --needed qrencode
+    sudo pacman -S --needed --noconfirm qrencode
     echo "qrencode has been installed."
 else
     echo "qrencode is already installed."
@@ -47,7 +68,7 @@ fi
 
 if ! command_exists polybar; then
     echo "Polybar not installed: installing now..."
-    sudo pacman -S --needed polybar
+    sudo pacman -S --needed --noconfirm polybar
     echo "Polybar has been installed."
 else
     echo "Polybar is already installed."
@@ -55,7 +76,7 @@ fi
 
 if ! command_exists picom; then
     echo "Picom not installed: installing now..."
-    sudo pacman -S --needed picom
+    sudo pacman -S --needed --noconfirm picom
     echo "Picom has been installed."
 else
     echo "Picom is already installed."
@@ -71,7 +92,7 @@ fi
 
 if ! command_exists rofi; then
     echo "Rofi is not installed: installing now..."
-    sudo pacman -S --needed rofi
+    sudo pacman -S --needed --noconfirm rofi
     echo "rofi has been installed."
 else
     echo "Rofi is already installed."
@@ -79,7 +100,7 @@ fi
 
 if ! command_exists dmenu; then
     echo "dmenu is not installed: installing now..."
-    sudo pacman -S --needed dmenu
+    sudo pacman -S --needed --noconfirm dmenu
     echo "dmenu has been installed."
 else
     echo "dmenu is already installed."
@@ -87,7 +108,7 @@ fi
 
 if ! command_exists neofetch; then
     echo "neofetch is not installed: installing now..."
-    sudo pacman -S --needed neofetch
+    sudo pacman -S --needed --noconfirm neofetch
     echo "neofetch has been installed"
 else
     echo "neofetch is already installed."
@@ -95,7 +116,7 @@ fi
 
 if ! command_exists nvim; then
     echo "nvim is not installed: installing now..."
-    sudo pacman -S --needed nvim
+    sudo pacman -S --needed --noconfirm nvim
     echo "nvim has been installed"
 else
     echo "nvim is already installed."
