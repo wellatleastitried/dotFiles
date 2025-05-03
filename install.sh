12a1ff2366dd9c30db1931e986c61198d0f610a6
walitsnb <jackson1swindell@gmail.com>
2025-05-02 20:20:10 -0500

    Updated configs

--- install.sh
+++ install.sh
@@ -20,69 +20,102 @@
 command_exists() {
     command -v "$1" &> /dev/null
 }
+which apt > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+    echo "Detected apt package manager"
+    sudo apt update -y
+    sudo apt upgrade -y
+    PACKAGE_MANAGER="apt"
+elif which pacman > /dev/null 2>&1; then
+    echo "Detected pacman package manager"
 
-sudo pacman -Syu --noconfirm
+    sudo pacman -Syu --noconfirm
+
+    PACKAGE_MANAGER="pacman"
+else
+    echo "Unsupported package manager. Exiting..."
+    exit 1
+fi
 
 if ! command_exists git; then
-    echo "git not installed: installing now..."
-    sudo pacman -S --needed --noconfirm git base-devel
+    if [ "$PACKAGE_MANAGER" = "apt" ]; then
+        echo "git not installed: installing now..."
+        sudo apt install -y git
+    elif [ "$PACKAGE_MANAGER" = "pacman" ]; then
+        echo "git not installed: installing now..."
+        sudo pacman -S --needed --noconfirm git base-devel
+    fi
     git config --global init.defaultBranch main
     echo "git has been installed."
 else
     echo "git is already installed."
 fi
 
-if ! command_exists yay; then
-    echo "yay not installed: installing now..."
-    if ping -c 4 google.com; then
-        echo "Pulling yay from arch repo..."
-        git clone https://aur.archlinux.org/yay.git
+if [ "$PACKAGE_MANAGER" = "pacman" ]; then
+    if ! command_exists yay; then
+        echo "yay not installed: installing now..."
+        if ping -c 4 google.com; then
+            echo "Pulling yay from arch repo..."
+            git clone https://aur.archlinux.org/yay.git
+        else
+            echo "Copying yay from /tmp"
+            echo "YAY DIRECTORY CONTENTS:" && ls /tmp/yay
+            echo "PWD IS $(pwd)" || echo "how the hell did this error, exiting..." && exit 1
+            mv /tmp/yay /tmp/dotFiles/yay || echo "yay not found in /tmp, exiting..." && exit 1
+        fi
+        echo "Current Directory contents:" && ls
+        cd yay || echo "Error installing yay, exiting..." && exit 1
+        makepkg -si || echo "Unable to make package for yay, exiting..." && exit 1
+        cd ..
+        sudo rm -rf yay || echo "Error removing files, exiting..." && exit 1
+        echo "yay has been installed."
     else
-        echo "Copying yay from /tmp"
-        echo "YAY DIRECTORY CONTENTS:" && ls /tmp/yay
-        echo "PWD IS $(pwd)" || echo "how the hell did this error, exiting..." && exit 1
-        mv /tmp/yay /tmp/dotFiles/yay || echo "yay not found in /tmp, exiting..." && exit 1
+        echo "yay is already installed."
     fi
-    echo "Current Directory contents:" && ls
-    cd yay || echo "Error installing yay, exiting..." && exit 1
-    makepkg -si || echo "Unable to make package for yay, exiting..." && exit 1
-    cd ..
-    sudo rm -rf yay || echo "Error removing files, exiting..." && exit 1
-    echo "yay has been installed."
-else
-    echo "yay is already installed."
 fi
 
 if ! command_exists perl; then
-    echo "Perl not installed: installing now..."
-    sudo pacman -S --needed --noconfirm perl perl-term-extendedcolor
+    if [ "$PACKAGE_MANAGER" = "apt" ]; then
+        echo "Perl not installed: installing now..."
+        sudo apt install -y perl libterm-readline-gnu-perl
+    elif [ "$PACKAGE_MANAGER" = "pacman" ]; then
+        echo "Perl not installed: installing now..."
+        sudo pacman -S --needed --noconfirm perl perl-term-extendedcolor
+    fi
     echo "Perl has been installed"
 else
     echo "Perl is already installed"
 fi
 
 if ! command_exists qrencode; then
-    echo "qrencode not installed: installing now..."
-    sudo pacman -S --needed --noconfirm qrencode
+    if [ "$PACKAGE_MANAGER" = "apt" ]; then
+        echo "qrencode not installed: installing now..."
+        sudo apt install -y qrencode
+    elif [ "$PACKAGE_MANAGER" = "pacman" ]; then
+        echo "qrencode not installed: installing now..."
+        sudo pacman -S --needed --noconfirm qrencode
+    fi
     echo "qrencode has been installed."
 else
     echo "qrencode is already installed."
 fi
 
-if ! command_exists polybar; then
-    echo "Polybar not installed: installing now..."
-    sudo pacman -S --needed --noconfirm polybar
-    echo "Polybar has been installed."
-else
-    echo "Polybar is already installed."
-fi
+if [ "$PACKAGE_MANAGER" = "pacman" ]; then
+    if ! command_exists polybar; then
+        echo "Polybar not installed: installing now..."
+        sudo pacman -S --needed --noconfirm polybar
+        echo "Polybar has been installed."
+    else
+        echo "Polybar is already installed."
+    fi
 
-if ! command_exists picom; then
-    echo "Picom not installed: installing now..."
-    sudo pacman -S --needed --noconfirm picom
-    echo "Picom has been installed."
-else
-    echo "Picom is already installed."
+    if ! command_exists picom; then
+        echo "Picom not installed: installing now..."
+        sudo pacman -S --needed --noconfirm picom
+        echo "Picom has been installed."
+    else
+        echo "Picom is already installed."
+    fi
 fi
 
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
@@ -94,32 +127,49 @@ else
 fi
 
 if ! command_exists rofi; then
-    echo "Rofi is not installed: installing now..."
-    sudo pacman -S --needed --noconfirm rofi
+    if [ "$PACKAGE_MANAGER" = "apt" ]; then
+        echo "Rofi is not installed: installing now..."
+        sudo apt install -y rofi
+    elif [ "$PACKAGE_MANAGER" = "pacman" ]; then
+        echo "Rofi is not installed: installing now..."
+        sudo pacman -S --needed --noconfirm rofi
+    fi
     echo "rofi has been installed."
 else
     echo "Rofi is already installed."
 fi
 
-if ! command_exists dmenu; then
-    echo "dmenu is not installed: installing now..."
-    sudo pacman -S --needed --noconfirm dmenu
-    echo "dmenu has been installed."
-else
-    echo "dmenu is already installed."
+if [ "$PACKAGE_MANAGER" = "pacman" ]; then
+    if ! command_exists dmenu; then
+        echo "dmenu is not installed: installing now..."
+        sudo pacman -S --needed --noconfirm dmenu
+        echo "dmenu has been installed."
+    else
+        echo "dmenu is already installed."
+    fi
 fi
 
 if ! command_exists neofetch; then
-    echo "neofetch is not installed: installing now..."
-    sudo pacman -S --needed --noconfirm neofetch
+    if [ "$PACKAGE_MANAGER" = "apt" ]; then
+        echo "neofetch is not installed: installing now..."
+        sudo apt install -y neofetch
+    elif [ "$PACKAGE_MANAGER" = "pacman" ]; then
+        echo "neofetch is not installed: installing now..."
+        sudo pacman -S --needed --noconfirm neofetch
+    fi
     echo "neofetch has been installed"
 else
     echo "neofetch is already installed."
 fi
 
 if ! command_exists nvim; then
-    echo "nvim is not installed: installing now..."
-    sudo pacman -S --needed --noconfirm nvim
+    if [ "$PACKAGE_MANAGER" = "apt" ]; then
+        echo "nvim is not installed: installing now..."
+        sudo apt install -y neovim
+    elif [ "$PACKAGE_MANAGER" = "pacman" ]; then
+        echo "nvim is not installed: installing now..."
+        sudo pacman -S --needed --noconfirm neovim
+    fi
     echo "nvim has been installed"
 else
     echo "nvim is already installed."
@@ -160,16 +210,20 @@ cp -b "$PROJECT_DIR/zsh/themes/cool.zsh" "$HOME/.oh-my-zsh/themes/cool.zsh" || {
 }
 
 # Copy i3 configs
-cp -rb "$PROJECT_DIR/i3" "$HOME/.config/i3" || {
-    echo "Error: Could not complete copy of i3 configs"
-    exit 1
-}
+if [ "$PACKAGE_MANAGER" = "pacman" ]; then
+    cp -rb "$PROJECT_DIR/i3" "$HOME/.config/i3" || {
+        echo "Error: Could not complete copy of i3 configs"
+        exit 1
+    }
+fi
 
 # Copy polybar configs
-cp -rb "$PROJECT_DIR/polybar" "$HOME/.config/polybar" || {
-    echo "Error: Could not complete copy of polybar configs"
-    exit 1
-}
+if [ "$PACKAGE_MANAGER" = "pacman" ]; then
+    cp -rb "$PROJECT_DIR/polybar" "$HOME/.config/polybar" || {
+        echo "Error: Could not complete copy of polybar configs"
+        exit 1
+    }
+fi
 
 # Copy rofi config
 cp -rb "$PROJECT_DIR/rofi" "$HOME/.config/rofi" || {
@@ -178,10 +232,12 @@ cp -rb "$PROJECT_DIR/rofi" "$HOME/.config/rofi" || {
 }
 
 # Copy picom config
-cp -rb "$PROJECT_DIR/picom" "$HOME/.config/picom" || {
-    echo "Error: Could not complete copy of picom config"
-    exit 1
-}
+if [ "$PACKAGE_MANAGER" = "pacman" ]; then
+    cp -rb "$PROJECT_DIR/picom" "$HOME/.config/picom" || {
+        echo "Error: Could not complete copy of picom config"
+        exit 1
+    }
+fi
 
 # Copy misc scripts
 cp -rb "$PROJECT_DIR/scripts" "$HOME/.config/scripts" || {
@@ -190,10 +246,12 @@ cp -rb "$PROJECT_DIR/scripts" "$HOME/.config/scripts" || {
 }
 
 # Copy i3blocks configs
-cp -rb "$PROJECT_DIR/i3blocks" "$HOME/.config/i3blocks" || {
-    echo "Error: Could not complete copy of i3blocks config and scripts"
-    exit 1
-}
+if [ "$PACKAGE_MANAGER" = "pacman" ]; then
+    cp -rb "$PROJECT_DIR/i3blocks" "$HOME/.config/i3blocks" || {
+        echo "Error: Could not complete copy of i3blocks config and scripts"
+        exit 1
+    }
+fi
 
 # Neofetch config
 cp -rb "$PROJECT_DIR/neofetch" "$HOME/.config/neofetch/config.conf" || {
@@ -208,7 +266,9 @@ cp -rb "$PROJECT_DIR/nvim" "$HOME/.config" || {
 }
 
 # Grub configuration
-sudo ./"$PROJECT_DIR/grub/GreyFocus/install.sh"
+if [ "$PACKAGE_MANAGER" = "pacman" ]; then
+    sudo ./"$PROJECT_DIR/grub/GreyFocus/install.sh"
+fi
 
 echo "Files have been successfully deployed."
 
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -5,7 +5,9 @@ if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
 	exec startx
 fi
 
-export EDITOR=code
+export EDITOR=nvim
+
+export PATH="$HOME/go/bin:$PATH"
 
 # Path to your oh-my-zsh installation.
 export ZSH="$HOME/.oh-my-zsh"
@@ -32,23 +34,14 @@ source "$ZSH/themes/cool.zsh"
 
 # Uncomment one of the following lines to change the auto-update behavior
 # zstyle ':omz:update' mode disabled  # disable automatic updates
-# zstyle ':omz:update' mode auto      # update automatically without asking
+zstyle ':omz:update' mode auto      # update automatically without asking
 # zstyle ':omz:update' mode reminder  # just remind me to update when it's time
 
 # Uncomment the following line to change how often to auto-update (in days).
-# zstyle ':omz:update' frequency 13
+zstyle ':omz:update' frequency 15
 
 # Uncomment the following line if pasting URLs and other text is messed up.
 # DISABLE_MAGIC_FUNCTIONS="true"
-
-# Uncomment the following line to disable colors in ls.
-# DISABLE_LS_COLORS="true"
-
-# Uncomment the following line to disable auto-setting terminal title.
-# DISABLE_AUTO_TITLE="true"
-
-# Uncomment the following line to enable command auto-correction.
-# ENABLE_CORRECTION="true"
 
 # Uncomment the following line to display red dots whilst waiting for completion.
 # You can also set it to another string to have that shown instead of the default red dots.
@@ -60,14 +53,6 @@ source "$ZSH/themes/cool.zsh"
 # under VCS as dirty. This makes repository status check for large repositories
 # much, much faster.
 # DISABLE_UNTRACKED_FILES_DIRTY="true"
-
-# Uncomment the following line if you want to change the command execution time
-# stamp shown in the history command output.
-# You can set one of the optional three formats:
-# "mm/dd/yyyy"|"dd.mm.yyyy"|"yyyy-mm-dd"
-# or set a custom format using the strftime function format specifications,
-# see 'man strftime' for details.
-# HIST_STAMPS="mm/dd/yyyy"
 
 # Would you like to use another custom folder than $ZSH/custom?
 # ZSH_CUSTOM=/path/to/new-custom-folder
@@ -85,24 +70,9 @@ plugins=(git)
 source $ZSH/oh-my-zsh.sh
 source $ZSH/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
-# User configuration
-
-# export MANPATH="/usr/local/man:$MANPATH"
-
 # Environment variables
 source $ZSH_CUSTOM/.env.zsh
 
-# You may need to manually set your language environment
-# export LANG=en_US.UTF-8
-
-# Preferred editor for local and remote sessions
-# if [[ -n $SSH_CONNECTION ]]; then
-#   export EDITOR='vim'
-# else
-#   export EDITOR='mvim'
-# fi
-
-# Compilation flags
 # export ARCHFLAGS="-arch x86_64"
 
 ####################################################################
